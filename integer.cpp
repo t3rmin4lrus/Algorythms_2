26f92be25b8d5bf7a9f6faaf561d8fef585e67aa
t3rmin4lrus <87004569+t3rmin4lrus@users.noreply.github.com>
2021-09-29 12:32:20 +0300

    Add files via upload

---
+++ integer.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	bool b = true;
+
+	cout << "Введите любое число (число 1 не является ни простым ни составным числом): " << endl;
+	cin >> a;
+
+	for (int i = 2; i <= sqrt(a); i++)
+	{
+		if (a % i == 0) 
+			b = false;
+	}
+
+	if (b == true)
+	{
+		cout << "Число является простым " << endl;
+	}
+	else
+	{
+		cout << "Число не является простым: " << endl;
+	}
+	return 0;
+}
+
